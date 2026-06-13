b29625f8cfda0f2d7b3e7dd01cc04f572d00ca69
sumit <bartwalsumit860@gmail.com>
2026-06-13 09:39:18 +0530

    fix again

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,4 +1,4 @@
-export const VITE_API_URL= "https://chit-chat-lf5q.onrender.com"
+export const VITE_API_URL= "https://chit-chat-lf5q.onrender.com/api/v1"
 
 export const USER_API_ENDPOINT =
 `${VITE_API_URL}/users`;
