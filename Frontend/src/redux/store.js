4f5bd3b35b5d2616e00d103b1a800e7d6fa97382
sumit <bartwalsumit860@gmail.com>
2026-06-13 09:51:19 +0530

    remove convesation from persist state

--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -26,7 +26,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key:"root",
   storage,
-  whitelist:["auth","conversation"]
+  whitelist:["auth"]
 };
 
 const persistedReducer = persistReducer(
